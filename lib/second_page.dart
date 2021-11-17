2f5dd9ffcfd6e0cd07c1dd6398fc22f7fb02b1ad
= <=>
2021-11-17 12:44:28 +0100

    new changes

--- lib/second_page.dart
+++ lib/second_page.dart
@@ -11,7 +11,40 @@ class SecondPage extends StatelessWidget {
       body: Padding(
           padding: EdgeInsets.all(20),
           child: Text(
-              'I am really happy to be building my own app with Flutter!I am really happy to be building my own app with Flutter!I am really happy to be building my own app with Flutter!I am really happy to be building my own app with Flutter!I am really happy to be building my own app with Flutter!I am really happy to be building my own app with Flutter!I am really happy to be building my own app with Flutter!I am really happy to be building my own app with Flutter!')),
+              'I am really happy to be building my own app with Flutter!!')),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Center(
+                  child: Text(
+                "Hello World",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 40,
+                    fontWeight: FontWeight.w900),
+              )),
+            ),
+            ListTile(
+              title: Text("Home"),
+              leading: Icon(Icons.home),
+            ),
+            ListTile(
+              title: Text("About"),
+              leading: Icon(Icons.info_outline_rounded),
+            ),
+            ListTile(
+              title: Text("Help"),
+              leading: Icon(Icons.access_alarms),
+            ),
+            ListTile(
+              title: Text("Settings"),
+              leading: Icon(Icons.settings),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
